b150cd96cd31cf69710f3bb49539aba898543648
Ryan Rahimi <rahimi_ry@student.kings.edu.au>
2025-04-12 17:34:24 +1000

    created barebones structure of all pages and general styles through respective css and html files for each page. Need to add interactive elements and make the '+' buttons work next

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__, template_folder="Frontend - HTML") # Specify template folder
+
+
+# Routes
+@app.route("/") # Immediately redirect to login from root
+def home():
+    return redirect(url_for("login"))
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        return render_template("Build.html")
+
+    return render_template("Login.html")
+
+@app.route("/build")
+def build():
+    return render_template("Build.html")
+
+@app.route("/saves")
+def saves():
+    return render_template("Save.html")
+
+@app.route("/recs")
+def recs():
+    return render_template("Recs.html")
+
+@app.route("/blackjack")
+def blackjack():
+    return render_template("Blackjack.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
